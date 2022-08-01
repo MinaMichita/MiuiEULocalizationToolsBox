57e44680ad23ca238f92b2f35db9368e9debb442
MinaMichita <minamichitago@gmail.com>
2022-08-01 14:14:29 +0800

    Update to v13.22.7.28

--- app/src/main/java/moe/minamigo/MiuiEuLocalization/MainActivity.java
+++ app/src/main/java/moe/minamigo/MiuiEuLocalization/MainActivity.java
@@ -13,19 +13,31 @@ import java.io.DataOutputStream;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
-    private TextView rootStateText, appVersionNameText, magiskModuleVersionText, xposedModuleStateText;
-
     private boolean isMagiskModuleInstalled = false, isRooted = false;
     private static boolean isXposedModuleEnable = false;
+
+    private TextView rootStateText, appVersionNameText, magiskModuleVersionText, xposedModuleStateText;
+
+    private String nonrootToastString;
+    private String processingToastString;
+    private String processSuccessedToastString;
+    private String processFailedToastString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         this.rootStateText = (TextView) findViewById(R.id.textView_root_state);
         this.magiskModuleVersionText = (TextView) findViewById(R.id.textView_magisk_module_version);
         this.appVersionNameText = (TextView) findViewById(R.id.textView_app_version_name);
         this.xposedModuleStateText = (TextView) findViewById(R.id.textView_xposed_module_state);
+
+        nonrootToastString = this.getString(R.string.mainactivity_toast_nonroot);
+        processingToastString = this.getString(R.string.mainactivity_toast_processing);
+        processSuccessedToastString = this.getString(R.string.mainactivity_toast_process_success);
+        processFailedToastString = this.getString(R.string.mainactivity_toast_process_failed);
+
         getDeviceStatus();
     }
 
@@ -51,69 +63,82 @@ public class MainActivity extends AppCompatActivity {
         getDeviceStatus();
     }
 
-    public void fixPermissionsHandler(View view) {
+    public void resetPermissionsHandler(View view) {
         if (!this.isRooted) {
-            Toast.makeText(this, "设备未root或未授权应用root权限！", Toast.LENGTH_SHORT).show();
-            return;
-        } else if (!isMagiskModuleInstalled) {
-            Toast.makeText(this, "未安装Magisk模块或版本过低！", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, nonrootToastString, Toast.LENGTH_SHORT).show();
             return;
         } else {
-            Toast.makeText(this, "开始修复，请等待一会！", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, processingToastString, Toast.LENGTH_SHORT).show();
         }
-        if (!rootCommand("sh /system/etc/localization/tools/repairPermissions.sh")) {
-            Toast.makeText(this, "执行失败！", Toast.LENGTH_SHORT).show();
+        if (!rootCommand("rm -f /data/misc_de/0/apexdata/com.android.permission/runtime-permissions.xml && reboot")) {
+            Toast.makeText(this, processFailedToastString, Toast.LENGTH_SHORT).show();
             return;
         }
-        Toast.makeText(this, "修复完成！", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, processSuccessedToastString, Toast.LENGTH_SHORT).show();
+    }
+    public void fixPermissionsHandler(View view) {
+        if (!this.isRooted) {
+            Toast.makeText(this, nonrootToastString, Toast.LENGTH_SHORT).show();
+            return;
+        } else if (!isMagiskModuleInstalled) {
+            Toast.makeText(this, this.getString(R.string.mainactivity_toast_not_magisk_module_installed), Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            Toast.makeText(this, processingToastString, Toast.LENGTH_SHORT).show();
+        }
+        if (!rootCommand("sh /system/etc/localization/tools/repairPermissions.sh")) {
+            Toast.makeText(this, processFailedToastString, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Toast.makeText(this, processSuccessedToastString, Toast.LENGTH_SHORT).show();
     }
 
     public void deleteErrorLogHandler(View view) {
         if (!this.isRooted) {
-            Toast.makeText(this, "设备未root或未授权应用root权限！", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, nonrootToastString, Toast.LENGTH_SHORT).show();
             return;
         }
         if(!rootCommand("rm -f /sdcard/MiuiEULocalization/ErrorLog.tar rm -rf /data/system/dropbox/*wtf@*")){
-            Toast.makeText(this, "执行失败！", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, processFailedToastString, Toast.LENGTH_SHORT).show();
             return;
         }
-        Toast.makeText(this, "清空错误日志完成", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, processSuccessedToastString, Toast.LENGTH_SHORT).show();
     }
 
     public void packageErrorLogHandler(View view) {
         if (!this.isRooted) {
-            Toast.makeText(this, "设备未root或未授权应用root权限！", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, nonrootToastString, Toast.LENGTH_SHORT).show();
             return;
         }
         if(!rootCommand("[ -d /sdcard/MiuiEULocalization ] || mkdir /sdcard/MiuiEULocalization && tar -cf /sdcard/MiuiEULocalization/ErrorLog.tar /data/system/dropbox/*wtf@* /system/build.prop /system/etc/localization")){
-            Toast.makeText(this, "执行失败！", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, processFailedToastString, Toast.LENGTH_SHORT).show();
             return;
         }
-        Toast.makeText(this, "打包完成，路径：/sdcard/MiuiEULocalization/ErrorLog.tar\n日志包含设备基本信息，请勿随意分享！", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, this.getString(R.string.mainactivity_toast_package_error_log_success), Toast.LENGTH_LONG).show();
     }
 
     public void hideThemeFolderHandler(View view) {
         if (!this.isRooted) {
-            Toast.makeText(this, "设备未root或未授权应用root权限！", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, nonrootToastString, Toast.LENGTH_SHORT).show();
             return;
         }
         if(!rootCommand("touch /sdcard/MIUI/theme/.nomedia && rm -rf /data/user/0/com.miui.gallery/databases/*gallery* && am force-stop com.miui.gallery")) {
-            Toast.makeText(this, "执行失败！", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, processFailedToastString, Toast.LENGTH_SHORT).show();
             return;
         }
-        Toast.makeText(this, "执行完成", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, processSuccessedToastString, Toast.LENGTH_SHORT).show();
     }
 
     public void allowSysAppUpdateHandler(View view) {
         if (!this.isRooted) {
-            Toast.makeText(this, "设备未root或未授权应用root权限！", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, nonrootToastString, Toast.LENGTH_SHORT).show();
             return;
         }
         if(!rootCommand("setprop persist.sys.allow_sys_app_update true")){
-            Toast.makeText(this, "执行失败！", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, processFailedToastString, Toast.LENGTH_SHORT).show();
             return;
         }
-        Toast.makeText(this, "执行完成", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, processSuccessedToastString, Toast.LENGTH_SHORT).show();
     }
 
     private void getDeviceStatus() {
@@ -125,32 +150,31 @@ public class MainActivity extends AppCompatActivity {
             this.appVersionNameText.setText(appVersion);
         } catch (Exception e) {
             e.printStackTrace();
-            this.appVersionNameText.setText("检查错误，请重试！");
+            this.appVersionNameText.setText(this.getString(R.string.mainactivity_text_check_app_version_failed));
         }
 
         String magiskModuleVersion = SystemPropertiesProxy.get(this, "ro.miui.EULocalization");
         if (magiskModuleVersion == null || magiskModuleVersion.equals("")) {
-            this.magiskModuleVersionText.setText("未安装或版本过旧");
+            this.magiskModuleVersionText.setText(this.getString(R.string.mainactivity_text_magisk_module_not_installed));
             this.isMagiskModuleInstalled = false;
         } else {
             this.magiskModuleVersionText.setText(magiskModuleVersion);
             this.isMagiskModuleInstalled = true;
             if (!appVersion.equals(magiskModuleVersion)) {
-                Toast.makeText(this, "警告！工具箱版本和模块版本不匹配！", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, this.getString(R.string.mainactivity_toast_magisk_module_tools_not_match), Toast.LENGTH_LONG).show();
             }
         }
 
         if (isXposedModuleEnable) {
-            this.xposedModuleStateText.setText("已激活！");
+            this.xposedModuleStateText.setText(this.getString(R.string.mainactivity_text_xposed_module_activated));
             return;
         } else {
-            this.xposedModuleStateText.setText("未激活！请检查");
+            this.xposedModuleStateText.setText(this.getString(R.string.mainactivity_text_xposed_module_inactivated));
         }
     }
 
     private Process process = null;
     private DataOutputStream os = null;
-
     private synchronized void getRootState() {
         try {
             process = Runtime.getRuntime().exec("su");
@@ -173,7 +197,6 @@ public class MainActivity extends AppCompatActivity {
             isRooted = false;
         }
     }
-
     private boolean rootCommand(String command) {
         if (process == null || os == null) {
             return false;
--- app/src/main/java/moe/minamigo/MiuiEuLocalization/MainModule.java
+++ app/src/main/java/moe/minamigo/MiuiEuLocalization/MainModule.java
@@ -1,13 +1,7 @@
 package moe.minamigo.MiuiEuLocalization;
 
-import android.app.AndroidAppHelper;
-import android.content.Context;
-import android.os.Build;
-import android.view.inputmethod.InputMethodManager;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
@@ -17,14 +11,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainModule implements IXposedHookLoadPackage {
-    List<String> XSPACE_INTRODUCE_APPS;
-    boolean isA10;
-    boolean isA11;
+    List<String> XSPACE_INTRODUCE_APPS = new ArrayList();
 
     public MainModule() {
-        ArrayList arrayList = new ArrayList();
-        this.XSPACE_INTRODUCE_APPS = arrayList;
-        arrayList.add("com.tencent.mm");
+        this.XSPACE_INTRODUCE_APPS.add("com.tencent.mm");
         this.XSPACE_INTRODUCE_APPS.add("com.tencent.mobileqq");
         this.XSPACE_INTRODUCE_APPS.add("com.sina.weibo");
         this.XSPACE_INTRODUCE_APPS.add("com.whatsapp");
@@ -32,7 +22,7 @@ public class MainModule implements IXposedHookLoadPackage {
         this.XSPACE_INTRODUCE_APPS.add("com.instagram.android");
     }
 
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         String pkg = lpparam.packageName;
 
         switch (pkg){
@@ -50,16 +40,16 @@ public class MainModule implements IXposedHookLoadPackage {
                     XposedBridge.log("Hooked " + pkg + " Error: " + e.toString());
                 }
                 break;
-            case "com.google.android.inputmethod.latin":
+            case "com.miui.home":
                 try{
-                    handleGboard(lpparam);
+                    handleMiuiHome(lpparam);
                 }catch(Exception e) {
                     XposedBridge.log("Hooked " + pkg + " Error: " + e.toString());
                 }
                 break;
-            case "com.miui.home":
+            case "com.android.systemui":
                 try{
-                    handleMiuiHome(lpparam);
+                    handleSystemUI(lpparam);
                 }catch(Exception e) {
                     XposedBridge.log("Hooked " + pkg + " Error: " + e.toString());
                 }
@@ -77,96 +67,75 @@ public class MainModule implements IXposedHookLoadPackage {
         }
     }
 
+    private void handleSystemUI(final XC_LoadPackage.LoadPackageParam lpparam) {
+        final Class<?> notificationUtilClass = XposedHelpers.findClassIfExists("com.android.systemui.statusbar.notification.NotificationUtil", lpparam.classLoader);
+        if(notificationUtilClass != null){
+            final Method[] notificationUtilClassDeclaredMethods = notificationUtilClass.getDeclaredMethods();
+            Method shouldSubstituteSmallIconMethod = null;
+            for (Method method : notificationUtilClassDeclaredMethods) {
+                if (method.getName().equals("shouldSubstituteSmallIcon")) {
+                    shouldSubstituteSmallIconMethod = method;
+                    break;
+                }
+            }
+            if (shouldSubstituteSmallIconMethod != null) {
+                XposedBridge.hookMethod(shouldSubstituteSmallIconMethod, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                        final Class<?> classBuild = XposedHelpers.findClass("com.miui.systemui.BuildConfig", lpparam.classLoader);
+                        XposedHelpers.setStaticBooleanField(classBuild, "IS_INTERNATIONAL", true);
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                        final Class<?> classBuild = XposedHelpers.findClass("com.miui.systemui.BuildConfig", lpparam.classLoader);
+                        XposedHelpers.setStaticBooleanField(classBuild, "IS_INTERNATIONAL", false);
+                    }
+                });
+            } else{
+                XposedBridge.log("Method not found: com.android.systemui.statusbar.notification.NotificationUtil.shouldSubstituteSmallIcon");
+            }
+        }else{
+            XposedBridge.log("Class not found: com.android.systemui.statusbar.notification.NotificationUtil");
+        }
+    }
+
     private void handleMiuiHome(XC_LoadPackage.LoadPackageParam lpparam) {
         handleInternational(lpparam);
 
-        Class<?> clazz_NotificationManagerServiceInjector = XposedHelpers.findClass("com.miui.home.launcher.MIUIWidgetUtil", lpparam.classLoader);
-        final Method[] declareMethods_NotificationManagerServiceInjector = clazz_NotificationManagerServiceInjector.getDeclaredMethods();
-        Method isMiuiWidgetSupportMethod = null;
-        for (Method method : declareMethods_NotificationManagerServiceInjector) {
-            if (method.getName().equals("isMIUIWidgetSupport")) {
-                isMiuiWidgetSupportMethod = method;
-                break;
+        Class<?> miuiWidgetUtilClass = XposedHelpers.findClassIfExists("com.miui.home.launcher.MIUIWidgetUtil", lpparam.classLoader);
+        if(miuiWidgetUtilClass != null){
+            final Method[] MiuiWidgetUtilClassDeclaredMethods = miuiWidgetUtilClass.getDeclaredMethods();
+            Method isMiuiWidgetSupportMethod = null;
+            for (Method method : MiuiWidgetUtilClassDeclaredMethods) {
+                if (method.getName().equals("isMIUIWidgetSupport")) {
+                    isMiuiWidgetSupportMethod = method;
+                    break;
+                }
             }
-        }
-        if (isMiuiWidgetSupportMethod != null) {
-            XposedBridge.hookMethod(isMiuiWidgetSupportMethod, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-                    handleChina(lpparam);
-                }
+            if (isMiuiWidgetSupportMethod != null) {
+                XposedBridge.hookMethod(isMiuiWidgetSupportMethod, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                        handleChina(lpparam);
+                    }
 
-                @Override
-                protected void afterHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-                    handleInternational(lpparam);
-                }
-            });
-        } else{
-            XposedBridge.log("isMiuiWidgetSupportMethod is not found!");
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                        handleInternational(lpparam);
+                    }
+                });
+            } else{
+                XposedBridge.log("Method not found: com.miui.home.launcher.MIUIWidgetUtil.isMIUIWidgetSupport");
+            }
+        }else{
+            XposedBridge.log("Class not found: com.miui.home.launcher.MIUIWidgetUtil");
         }
     }
 
     private void handleSecuritycore(XC_LoadPackage.LoadPackageParam lpparam) {
         handleInternational(lpparam);
         XposedHelpers.setStaticObjectField(XposedHelpers.findClass("com.miui.xspace.constant.XSpaceApps", lpparam.classLoader), "XSPACE_INTRODUCE_APPS", this.XSPACE_INTRODUCE_APPS);
-    }
-
-    public void checkVersion() {
-        switch (Build.VERSION.SDK_INT) {
-            case 28:
-            case 29:
-                this.isA10 = true;
-                this.isA11 = false;
-                return;
-            case 30:
-                this.isA10 = false;
-                this.isA11 = true;
-                return;
-            default:
-                this.isA10 = false;
-                this.isA11 = false;
-                return;
-        }
-    }
-    private void handleGboard(XC_LoadPackage.LoadPackageParam lpparam) {
-        checkVersion();
-        final Class<?> clazz = XposedHelpers.findClass("android.inputmethodservice.InputMethodServiceInjector", lpparam.classLoader);
-        XposedHelpers.setStaticIntField(clazz, "sIsImeSupport", 1);
-        if (this.isA10) {
-            XposedHelpers.findAndHookMethod(clazz, "isXiaoAiEnable", new Object[]{XC_MethodReplacement.returnConstant(false)});
-        } else {
-            XposedHelpers.findAndHookMethod("com.android.internal.policy.PhoneWindow", lpparam.classLoader, "setNavigationBarColor", new Object[]{Integer.TYPE, new XC_MethodHook() {
-                /* class moe.minamigo.MiuiEULocalization.MainModule.AnonymousClass1 */
-
-                /* access modifiers changed from: protected */
-                public void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-                    int color = -1 - ((Integer) param.args[0]).intValue();
-                    XposedHelpers.callStaticMethod(clazz, "customizeBottomViewColor", new Object[]{true, param.args[0], Integer.valueOf(-16777216 | color), Integer.valueOf(1711276032 | color)});
-                }
-            }});
-        }
-        if (this.isA10) {
-            XposedHelpers.findAndHookMethod("android.inputmethodservice.InputMethodServiceInjector$MiuiSwitchInputMethodListener", lpparam.classLoader, "deleteNotSupportIme", new Object[]{XC_MethodReplacement.returnConstant((Object) null)});
-            return;
-        }
-        XposedHelpers.findAndHookMethod("android.inputmethodservice.InputMethodModuleManager", lpparam.classLoader, "loadDex", new Object[]{ClassLoader.class, String.class, new XC_MethodHook() {
-            /* class moe.minamigo.MiuiEULocalization.MainModule.AnonymousClass2 */
-
-            /* access modifiers changed from: protected */
-            public void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-                final Class<?> clazz = XposedHelpers.findClass("com.miui.inputmethod.InputMethodBottomManager", (ClassLoader) param.args[0]);
-                XposedHelpers.setStaticIntField(clazz, "sIsImeSupport", 1);
-                XposedHelpers.findAndHookMethod(clazz, "isXiaoAiEnable", new Object[]{XC_MethodReplacement.returnConstant(false)});
-                XposedHelpers.findAndHookMethod(clazz, "getSupportIme", new Object[]{new XC_MethodReplacement() {
-                    /* class moe.minamigo.MiuiEULocalization.MainModule.AnonymousClass2.AnonymousClass1 */
-
-                    /* access modifiers changed from: protected */
-                    public Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-                        return ((InputMethodManager) XposedHelpers.getObjectField(XposedHelpers.getStaticObjectField(clazz, "sBottomViewHelper"), "mImm")).getEnabledInputMethodList();
-                    }
-                }});
-            }
-        }});
     }
 
     private void handleSelf(XC_LoadPackage.LoadPackageParam lpparam) {
